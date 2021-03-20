720f678cb8ab5c77d33fa04e410c2ff243efacc6
xion6 <u.xion6@gmail.com>
2021-03-20 21:46:21 +0900

    Import css files

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,41 @@
  * webpack.config.js
  */
 
- const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
- module.exports = {
-   mode: process.env.NODE_ENV || 'development',
-   entry: './src/app.js',
-   output: {
-     filename: 'main.js',
-     path: path.resolve(__dirname, 'dist')
-   },
-   module: {
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: "./src/app.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
     rules: [
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css/,
+        use: [
+          "style-loader",
+          { 
+            loader: "css-loader", 
+            options: { 
+              url: false,
+            } 
+          },
+        ],
+      },
+    ],
   },
-  plugins: [
-    new HtmlWebpackPlugin()
-  ]
- }
+  plugins: [new HtmlWebpackPlugin()],
+};
